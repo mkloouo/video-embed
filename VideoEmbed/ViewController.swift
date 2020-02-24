3675a0412cac437518b89c9e9473b046790058f4
Mykola Odnosumov <mykolao@wix.com>
2020-02-24 18:36:40 +0200

    Initial Commit

---
+++ VideoEmbed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoEmbed
+//
+//  Created by Mykola Odnosumov on 24.02.2020.
+//  Copyright © 2020 Mykola Odnosumov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
