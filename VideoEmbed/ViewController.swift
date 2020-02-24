1a39717fef7019a448aef7ac757182f9ff16355f
Mykola Odnosumov <mykolao@wix.com>
2020-02-24 22:37:21 +0200

    add controls

--- VideoEmbed/ViewController.swift
+++ VideoEmbed/ViewController.swift
@@ -10,75 +10,196 @@ import UIKit
 import WebKit
 import JavaScriptCore
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKScriptMessageHandler {
+    
+    enum VideoState: String {
+        case playing = "Pause"
+        case paused = "Play"
+    }
+    
+    var videoState: VideoState = .paused
+    
     var webView: WKWebView!
     var slider: UISlider!
     var playButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        self.view.backgroundColor = .white
+        view.backgroundColor = .white
         
         let videoUrl = URL(string: "https://www.wix.com/video-embed?social=weed")
         let videoRequest = URLRequest(url: videoUrl!)
-        self.webView = WKWebView()
-        self.webView.load(videoRequest)
         
-        self.webView.backgroundColor = .white
+        let webViewConfig = WKWebViewConfiguration()
+        webViewConfig.allowsInlineMediaPlayback = true
+        webViewConfig.mediaTypesRequiringUserActionForPlayback = []
+        webViewConfig.userContentController.add(self, name: "jsHandler")
         
-        self.webView.layer.borderWidth = 1
-        self.webView.layer.borderColor = UIColor(ciColor: .black).cgColor
+        webView = WKWebView(frame: .zero, configuration: webViewConfig)
+        webView.load(videoRequest)
         
-        self.webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.evaluateJavaScript("""
+let checkVideoPlayerId;
+
+function updateSliderConstraints() {
+    sendToNative({
+        initialSliderConfiguration: {
+            duration: window.player.getDuration(),
+            currentTime: window.player.getCurrentTime()
+        }
+    });
+}
+
+let playing = false;
+
+function handleUpdateSlider() {
+    sendToNative({
+        updateSlider: {
+            playing,
+            currentTime: window.player.getCurrentTime()
+        }
+    });
+}
+
+function playVideo() {
+    window.player.play();
+
+    if (!playing) playing = true;
+    sliderUpdateId = setInterval(handleUpdateSlider, 1000);
+}
+
+function pauseVideo() {
+    window.player.pause();
+
+    if (playing) playing = false;
+    clearInterval(sliderUpdateId);
+}
+
+function seekVideo(sec) {
+    window.player.seek(sec);
+
+//    if (!playing) playing = true;
+//    handleUpdateSlider();
+}
+
+function checkVideoPlayer() {
+    if (window.player) {
+        clearInterval(checkVideoPlayerId);
+        sendToNative('video player available');
+        sendToNative('seek to: ' + Object.keys(window.player).join(','))
+        updateSliderConstraints();
+
+        return;
+    }
+ 
+    sendToNative('checked for video player');
+}
+
+window.onload = () => {
+    checkVideoPlayerId = setInterval(checkVideoPlayer, 1000);
+}
+
+function sendToNative(object) {
+    window.webkit.messageHandlers.jsHandler.postMessage(object);
+}
+""");
         
-        self.view.addSubview(webView)
+        webView.backgroundColor = .white
         
-        self.webView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
-        self.webView.heightAnchor.constraint(equalToConstant: self.view.frame.size.height / 2).isActive = true
+        webView.layer.borderWidth = 1
+        webView.layer.borderColor = UIColor(ciColor: .black).cgColor
         
-        self.webView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        self.webView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        webView.translatesAutoresizingMaskIntoConstraints = false
         
-        self.slider = UISlider()
-        self.slider.minimumValue = 0
-        self.slider.maximumValue = 100
-        self.slider.value = 31.0
-        self.slider.addTarget(self, action: #selector(handleSliderDragInside), for: .touchDragInside)
+        view.addSubview(webView)
         
-        self.slider.translatesAutoresizingMaskIntoConstraints = false
+        webView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        webView.heightAnchor.constraint(equalToConstant: view.frame.size.height / 2).isActive = true
         
-        self.view.addSubview(self.slider)
+        webView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         
-        self.slider.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 20).isActive = true
-        self.slider.widthAnchor.constraint(equalTo: self.view.widthAnchor, constant: -40).isActive = true
+        slider = UISlider()
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.value = 31.0
+        slider.addTarget(self, action: #selector(handleSliderDragInside), for: .touchDragInside)
         
-        self.slider.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        self.slider.topAnchor.constraint(equalTo: self.webView.bottomAnchor, constant: 20).isActive = true
+        slider.translatesAutoresizingMaskIntoConstraints = false
         
-        self.playButton = UIButton(type: .system)
-        self.playButton.setTitle("Play", for: .normal)
-        self.playButton.addTarget(self, action: #selector(handlePlayButtonClick), for: .touchUpInside)
+        view.addSubview(slider)
         
-        self.playButton.translatesAutoresizingMaskIntoConstraints = false
+        slider.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        slider.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40).isActive = true
         
-        self.view.addSubview(self.playButton)
+        slider.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        slider.topAnchor.constraint(equalTo: webView.bottomAnchor, constant: 20).isActive = true
         
-        self.playButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        self.playButton.topAnchor.constraint(equalTo: self.slider.bottomAnchor, constant: 20).isActive = true
+        playButton = UIButton(type: .system)
+        playButton.setTitle(videoState.rawValue, for: .normal)
+        playButton.addTarget(self, action: #selector(handlePlayButtonClick), for: .touchUpInside)
+        
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(playButton)
+        
+        playButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        playButton.topAnchor.constraint(equalTo: slider.bottomAnchor, constant: 20).isActive = true
     }
     
     @objc
     func handleSliderDragInside() {
-        print("slider value: \(self.slider.value)")
+        webView.evaluateJavaScript("seekVideo(\(slider.value))")
     }
     
     @objc
     func handlePlayButtonClick() {
-        print("clicked button")
+        switch (videoState) {
+        case .playing:
+            videoState = .paused
+            webView.evaluateJavaScript("pauseVideo()")
+        case .paused:
+            videoState = .playing
+            webView.evaluateJavaScript("playVideo()")
+        }
+        
+        playButton.setTitle(videoState.rawValue, for: .normal)
     }
-
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if (message.name == "jsHandler") {
+            if let body = message.body as? NSDictionary {
+                
+                if let initialConfig = body.value(forKey: "initialSliderConfiguration") as? NSDictionary {
+                    if let duration = initialConfig.value(forKey: "duration") as? NSNumber {
+                        slider.maximumValue = duration.floatValue
+                    }
+                    if let currentTime = initialConfig.value(forKey: "currentTime") as? NSNumber {
+                        slider.value = currentTime.floatValue
+                    }
+                    
+                    return
+                }
+                
+                if let updateSlider = body.value(forKey: "updateSlider") as? NSDictionary {
+                    if let playing = updateSlider.value(forKey: "playing") as? Bool {
+                        videoState = playing ? .playing : .paused
+                        playButton.setTitle(videoState.rawValue, for: .normal)
+                    }
+                    if let currentTime = updateSlider.value(forKey: "currentTime") as? NSNumber {
+                        slider.value = currentTime.floatValue
+                    }
+                    
+                    return
+                }
+                
+                
+            }
+            
+            print(message.body)
+        }
+    }
+    
 }
 
